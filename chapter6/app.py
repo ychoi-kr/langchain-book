aa57d4295558063c4444690974d44d5991f8c509
Yong Choi <sk8er.choi@gmail.com>
2024-04-29 18:56:09 +0900

    Remove redundant comment The deleted comment will be described in the book.

--- chapter6/app.py
+++ chapter6/app.py
@@ -11,47 +11,42 @@ from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
-
 def create_agent_chain(history):
     chat = ChatOpenAI(
         model_name=os.environ["OPENAI_API_MODEL"],
         temperature=os.environ["OPENAI_API_TEMPERATURE"],
     )
-
+    
     tools = load_tools(["ddg-search", "wikipedia"])
-
+    
     prompt = hub.pull("hwchase17/openai-tools-agent")
-
+    
     memory = ConversationBufferMemory(
         chat_memory=history, memory_key="chat_history", return_messages=True
     )
-
+    
     agent = create_openai_tools_agent(chat, tools, prompt)
     return AgentExecutor(agent=agent, tools=tools, memory=memory)
 
-
 st.title("langchain-streamlit-app")
 
-# StreamlitChatMessageHistoryを使用することで、
-# st.session_stateを使った会話履歴の管理を自前で実装する必要がなくなりました。
 history = StreamlitChatMessageHistory()
 
 for message in history.messages:
     st.chat_message(message.type).write(message.content)
-
+    
 prompt = st.chat_input("What is up?")
 
 if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
-
+        
     with st.chat_message("assistant"):
         callback = StreamlitCallbackHandler(st.container())
-
         agent_chain = create_agent_chain(history)
         response = agent_chain.invoke(
             {"input": prompt},
             {"callbacks": [callback]},
         )
-
+        
         st.markdown(response["output"])
